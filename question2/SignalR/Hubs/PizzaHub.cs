3fc69decaa4122037e4e975643ff5e0327999f3e
WilliamOsuji <williamosuji1680@gmail.com>
2024-10-07 09:56:54 -0400

    MVC Question #2

--- question2/SignalR/Hubs/PizzaHub.cs
+++ question2/SignalR/Hubs/PizzaHub.cs
@@ -7,6 +7,11 @@ namespace SignalR.Hubs
     {
         private readonly PizzaManager _pizzaManager;
 
+        public string groupName;
+        public int nbUsers;
+        public int Money;
+        public int NbPizza;
+
         public PizzaHub(PizzaManager pizzaManager) {
             _pizzaManager = pizzaManager;
         }
@@ -14,27 +19,43 @@ namespace SignalR.Hubs
         public override async Task OnConnectedAsync()
         {
             await base.OnConnectedAsync();
+            _pizzaManager.AddUser();
+            nbUsers = _pizzaManager.NbConnectedUsers;
+            await Clients.All.SendAsync(nbUsers.ToString());
         }
 
         public override async Task OnDisconnectedAsync(Exception? exception)
         {
             await base.OnConnectedAsync();
+            _pizzaManager.RemoveUser();
+            nbUsers = _pizzaManager.NbConnectedUsers;
+            await Clients.All.SendAsync(nbUsers.ToString());
         }
 
         public async Task SelectChoice(PizzaChoice choice)
         {
+            groupName = _pizzaManager.GetGroupName(choice);
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
         }
 
         public async Task UnselectChoice(PizzaChoice choice)
         {
+            groupName = _pizzaManager.GetGroupName(choice);
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
         }
 
         public async Task AddMoney(PizzaChoice choice)
         {
+            groupName = _pizzaManager.GetGroupName(choice);
+            _pizzaManager.IncreaseMoney(choice);
+            await Clients.Group(groupName).SendAsync(_pizzaManager.NbPizzas.ToString());
         }
 
         public async Task BuyPizza(PizzaChoice choice)
         {
+            groupName = _pizzaManager.GetGroupName(choice);
+            _pizzaManager.BuyPizza(choice);
+            await Clients.Group(groupName).SendAsync(_pizzaManager.Money.ToString());
         }
     }
 }
